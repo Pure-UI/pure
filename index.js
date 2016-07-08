332d31a59b8fa4bbba56190362ee003075af4426
mo <feng_zilong@163.com>
2016-07-08 20:07:29 +0800

    :zap:updated

---
+++ index.js
@@ -0,0 +1,12 @@
+import '../dist/pure.js';
+import '../dist/pure.css';
+import { Router } from 'director';
+import App from './app';
+import routes from './routes';
+
+new App().$inject( document.getElementById( 'app' ) );
+
+const router = Router( routes );
+router.setRoute( 'Button' );
+
+router.init();
