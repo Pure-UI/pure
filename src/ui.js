bceb3d1b09c79e7edacb44149e7523f6cc725462
mo <feng_zilong@163.com>
2016-08-18 23:46:01 +0800

    Removed layout in favor of Box

--- src/ui.js
+++ src/ui.js
@@ -1,69 +1,44 @@
 // Basic
-// button
 import './components/button/button';
-// icon
 import './components/icon/icon';
-// spinner
 import './components/spinner/spinner';
 
 // Navigation
-// pagination
 import './components/pagination/pagination';
-// breadcrumb
 import './components/breadcrumb/breadcrumb';
 import './components/breadcrumb/breadcrumb-item';
 
-// Tree
-// jsontree
-import './components/json-tree/json-tree';
-
 // Form
-// input
 import './components/input/input';
-// textarea
 import './components/textarea/textarea';
-// radio
 import './components/radio/radio';
 import './components/radio/radio-group';
-// checkbox
 import './components/checkbox/checkbox';
 import './components/checkbox/checkbox-group';
-// select
 import './components/select/select';
-// switch
 import './components/switch/switch';
-// Form
 import './components/form/form';
 import './components/form/form-item';
 import './components/form/form-label';
 import './components/form/form-control';
-// table
 import './components/table/table';
 import './components/table/table-row';
 import './components/table/tr';
 import './components/table/td';
 
 // Container
-// modal
 import './components/modal/modal';
-// box
 import './components/box/box';
 
 // Message
-// Note
 import './components/note/note';
 
 // Layout
-// Center
-import './components/layout/flex';
-import './components/layout/center';
 
 // Locate
-// Track
 import './components/track/track';
 
 // Time
-// Countdown
 import './components/countdown/countdown';
 
 // Tree
@@ -71,3 +46,4 @@ import './components/element-tree/element-tree';
 import './components/element-tree/element-tree-node';
 import './components/element-tree/element-tree-node-open';
 import './components/element-tree/element-tree-node-close';
+import './components/json-tree/json-tree';
