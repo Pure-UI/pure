d0b154e10fad9898580d8b4849e67ec56ebacc21
mo <feng_zilong@163.com>
2016-10-16 15:30:41 +0800

    compatible with regularjs@0.4.0

--- src/fn.js
+++ src/fn.js
@@ -1,18 +1,9 @@
-export default ( Component, v ) => {
-	v.note = ( content, type, timeout ) => {
+export default ( Component, Pure ) => {
+	Pure.note = ( content, type, timeout ) => {
 		const Note = Component.extend({
 			template: `
 				{#if showNote}
-				<div
-					class="r-note-will-transition"
-					r-animation="
-						on: enter;
-						wait: 10;
-						class: r-note-in, 3;
-						wait: ${timeout || 2000};
-						class: r-note-out, 3;
-						emit: remove;
-					">
+				<div class="r-note-will-transition" r-animation="on: enter;wait: 10;class: r-note-in, 3;wait: ${timeout || 2000};class: r-note-out, 3;emit: remove;">
 					<Note type="${type}">${content}</Note>
 				</div>
 				{/if}
