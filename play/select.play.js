72080896799fbbc0cfd2417aeec6a55ca7129985
mo <feng_zilong@163.com>
2017-03-11 15:25:52 +0800

    Click to toggle options visibility for Select

---
+++ play/select.play.js
@@ -0,0 +1,13 @@
+import Select from 'pure-select';
+import Option from 'pure-option';
+
+play( Select, module )
+	.name( 'Select' )
+	.component( 'Option', Option )
+	.add( 'basic', `
+		<Select>
+			<Option value="0">Option 0</Option>
+			<Option value="1">Option 1</Option>
+			<Option value="2">Option 2</Option>
+		</Select>
+	` )
