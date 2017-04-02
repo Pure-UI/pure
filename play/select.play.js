999ffcea06fdbbd5a914be8595520ed9efcfa19f
MO <fengzilong1992@gmail.com>
2017-04-03 01:49:17 +0800

    Improve Select style && move ProgressCircle stroke color definition into css (#65)

--- play/select.play.js
+++ play/select.play.js
@@ -5,7 +5,7 @@ play( Select, module )
 	.name( 'Select' )
 	.component( 'Option', Option )
 	.add( 'basic', `
-		<Select>
+		<Select selected="0">
 			<Option value="0">Option 0</Option>
 			<Option value="1">Option 1</Option>
 			<Option value="2">Option 2</Option>
