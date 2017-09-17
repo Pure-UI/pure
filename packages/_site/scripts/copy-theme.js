c4301b47568cd89af9aee106215f96e96cd02e2b
MO <fengzilong1992@gmail.com>
2017-09-17 20:26:28 +0800

    Keep only one theme in repo

--- packages/_site/scripts/copy-theme.js
+++ packages/_site/scripts/copy-theme.js
@@ -2,13 +2,7 @@ const copy = require( 'kopy' );
 const path = require( 'path' );
 
 copy(
-	path.resolve( __dirname, '../../_theme-default/lib' ),
+	path.resolve( __dirname, '../../_theme/lib' ),
 	path.resolve( __dirname, '../public/theme/default' ),
 	{}
 );
-
-copy(
-	path.resolve( __dirname, '../../_theme-ocean/lib' ),
-	path.resolve( __dirname, '../public/theme/ocean' ),
-	{}
-)
--- packages/_theme-ocean/src/index.js
+++
@@ -1,7 +0,0 @@
-import './fonts/iconfont.css';
-
-function load( requireContext ) {
-	return requireContext.keys().map( requireContext );
-}
-
-load( require.context( './components', true, /\.less$/ ) );
