0b269921735ffdd20d8fc384f38af9a596c6e068
MO <fengzilong1992@gmail.com>
2017-10-09 23:50:43 +0800

    refactor(NumberInput): remove size and add class

--- play/number-input.play.js
+++ play/number-input.play.js
@@ -3,6 +3,6 @@ import NumberInput from '@pure/number-input';
 play( NumberInput, module ).name( 'NumberInput' ).add(
 	'basic',
 	`
-		<NumberInput step=".5" value="10"></NumberInput>
+		<NumberInput step="{ .5 }" value="{ 5 }" min="{ 0 }" max="{ 10 }"></NumberInput>
 	`,
 );
