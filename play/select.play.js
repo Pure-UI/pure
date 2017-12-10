50ac32eab65a25c1d4f52073c596f2572c4bdc9c
MO <fengzilong1992@gmail.com>
2017-12-10 22:40:17 +0800

    feat(select): add animation for select
    fix #54

--- play/select.play.js
+++ play/select.play.js
@@ -4,7 +4,7 @@ import Option from '@pure/option';
 play( Select, module ).name( 'Select' ).component( 'Option', Option ).add(
 	'basic',
 	`
-		<Select selected="0">
+		<Select>
 			<Option value="0">Option 0</Option>
 			<Option value="1">Option 1</Option>
 			<Option value="2">Option 2</Option>
