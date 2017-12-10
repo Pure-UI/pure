6827435865ac816b84858d1fe9daf4bbebc1c958
MO <fengzilong1992@gmail.com>
2017-12-10 21:40:14 +0800

    fix(number-input): no change event is emitted when type 0 manually
    fix #87

--- play/number-input.play.js
+++ play/number-input.play.js
@@ -2,7 +2,14 @@ import NumberInput from '@pure/number-input';
 
 play( NumberInput, module ).name( 'NumberInput' ).add(
 	'basic',
-	`
-		<NumberInput step="{ .5 }" value="{ 5 }" min="{ 0 }" max="{ 10 }"></NumberInput>
-	`,
+	{
+		template: `
+			<NumberInput step="{ .5 }" value="{ 5 }" min="{ 0 }" max="{ 10 }" on-change="{ this.onChange($event) }"></NumberInput>
+		`,
+		onChange( v ) {
+			this.$log( typeof v )
+			this.$log( v )
+			this.$log( '-----' )
+		}
+	},
 );
